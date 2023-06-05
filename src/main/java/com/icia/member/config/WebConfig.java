d0c3b127fe41c9b5b4927a53eb55f919cac4730f
babinhi <klyuio@naver.com>
2023-06-05 15:46:52 +0900

    로그인 인터셉트 (axios 사용?)

--- src/main/java/com/icia/member/config/WebConfig.java
+++ src/main/java/com/icia/member/config/WebConfig.java
@@ -11,7 +11,7 @@ public class WebConfig implements WebMvcConfigurer {
         registry.addInterceptor(new LoginCheckInterceptor()) // 인터셉터로 등록할 클래스
                 .order(1) // 해당 인터셉터의 우선순위
                 .addPathPatterns("/**") // 인터셉터로 체크할 주소(모든주소)
-                .excludePathPatterns("/", "/member/save", "/member/login",
+                .excludePathPatterns("/", "/member/save", "/member/login", "/member/login/axios",
                         "/js/**", "/css/**", "/images/**",
                         "/*.ico", "/favicon/**"); // 인터셉터 검증을 하지 않을 주소
     }
